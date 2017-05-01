b0cee41b1ed9da07a49ccfcdd8f6f9709c846907
tonylixu@gmail.com <tonylixu@gmail.com>
2017-05-01 11:18:50 -0400

    Use ngResource instead of http services

--- app/core/core.module.js
+++ app/core/core.module.js
@@ -1,3 +1,4 @@
 'use strict';
 
-angular.module('core', []);
+// Add the core.phone module as a dependency of the core module
+angular.module('core', ['core.phone']);
--- app/core/phone/phone.service.js
+++ app/core/phone/phone.service.js
@@ -2,6 +2,8 @@
 
 angular.
   module('core.phone').
+  // The factory function is similar to a controller's constructor in that both
+  // can declare dependencies to be injected via function arguments.
   factory('Phone', ['$resource',
     function($resource) {
       return $resource('phones/:phoneId.json', {}, {
